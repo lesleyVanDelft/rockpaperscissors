6ddae301b740cd0f88ed0f3248de09e39d069842
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-12-02 19:19:58 +0100

    add button styling

--- src/components/PickButton/Button.jsx
+++ src/components/PickButton/Button.jsx
@@ -1,19 +1,37 @@
 import iconRock from '../../assets/images/icon-rock.svg';
 import iconPaper from '../../assets/images/icon-paper.svg';
 import iconScissors from '../../assets/images/icon-scissors.svg';
+import { useState } from 'react';
 
 const Button = () => {
-	const images = [iconRock, iconPaper, iconScissors];
+	const imagesTop = [iconPaper, iconScissors];
+	const imagesBot = [iconRock];
+	const [color, setColor] = useState('');
 
 	return (
 		<div className="ButtonChoice">
-			{images?.map((el, i) => {
-				return (
-					<button key={i} className={`choice ${i + 1}`}>
-						<img src={el} alt="dsfsd" />
-					</button>
-				);
-			})}
+			<div className="ButtonChoice__top">
+				{imagesTop?.map((el, i) => {
+					return (
+						<button
+							key={i}
+							className={`ButtonChoice__choice ${
+								i === 0 ? 'blue' : 'yellow'
+							} `}>
+							<img src={el} alt="dsfsd" />
+						</button>
+					);
+				})}
+			</div>
+			<div className="ButtonChoice__bot">
+				{imagesBot?.map((el, i) => {
+					return (
+						<button key={i} className={`ButtonChoice__choice red`}>
+							<img src={el} alt="dsfsd" />
+						</button>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
